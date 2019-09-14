c74abc5ed69d5315496da3f724f00d3212f77d95
Raisa <ramalia@student.unimelb.edu.au>
2019-09-14 23:35:44 +1000

    fix boundries, cam perspective, add bottom boundry

--- Project1/Assets/CameraMovements.cs
+++ Project1/Assets/CameraMovements.cs
@@ -4,47 +4,56 @@ using UnityEngine;
 
 public class CameraMovements : MonoBehaviour
 {
-    // add RigidBody to angle the direction of the forwards and backwards movement
-    // of the camera
-    public Rigidbody myRigidBody;
+  // add RigidBody to angle the direction of the forwards and backwards movement
+  // of the camera
+  public Rigidbody myRigidBody;
 
-    public float cameraSpeed = 0.1f;
-    public float rotationSpeed = 2.0f;
+  public float cameraSpeed = 0.1f;
+  public float rotationSpeed = 1.0f;
 
-    public float pitchRotation = 0.0f;
-    public float yawRotation = 0.0f;
-
-
-    private void Start(){
-
-        // Initialize RigidBody
-        myRigidBody = GetComponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
-    void Update(){
-        // Forward and Backward relative to where you are looking
-        if (Input.GetKey(KeyCode.W))
-            myRigidBody.AddForce(transform.forward * cameraSpeed);
-
-        if (Input.GetKey(KeyCode.S))
-            myRigidBody.AddForce((transform.forward*-1) * cameraSpeed);
-            
-        //Left and right
-        if (Input.GetKey(KeyCode.D))
-            myRigidBody.AddForce(transform.right * cameraSpeed);
-        if (Input.GetKey(KeyCode.A))
-            myRigidBody.AddForce(transform.right*-1 * cameraSpeed);
-
-        // Control relative pitch and yaw
-        // pitch = rotate in x-axis -> look up and down
-        pitchRotation -= rotationSpeed * Input.GetAxis("Mouse Y");
-
-        // yaw = rotate in y-axis -> look left and right
-        yawRotation += rotationSpeed * Input.GetAxis("Mouse X");
-
-        this.transform.eulerAngles = new Vector3(pitchRotation, yawRotation, 0.0f);
+  public float pitchRotation = 0.0f;
+  public float yawRotation = 0.0f;
 
 
-    }
+  private void Start()
+  {
+
+    // Initialize RigidBody
+    myRigidBody = GetComponent<Rigidbody>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    // Forward and Backward relative to where you are looking
+    if (Input.GetKey(KeyCode.W))
+      myRigidBody.AddForce(transform.forward * cameraSpeed);
+
+    if (Input.GetKey(KeyCode.S))
+      myRigidBody.AddForce((transform.forward * -1) * cameraSpeed);
+
+    //Left and right
+    if (Input.GetKey(KeyCode.D))
+      myRigidBody.AddForce(transform.right * cameraSpeed);
+    if (Input.GetKey(KeyCode.A))
+      myRigidBody.AddForce(transform.right * -1 * cameraSpeed);
+
+
+
+    // Can only rotate the camera if mouse is on screen
+
+    // Control relative pitch and yaw
+    // pitch = rotate in x-axis -> look up and down
+    pitchRotation -= rotationSpeed * Input.GetAxis("Mouse Y");
+
+    // yaw = rotate in y-axis -> look left and right
+    yawRotation += rotationSpeed * Input.GetAxis("Mouse X");
+
+    this.transform.eulerAngles = new Vector3(pitchRotation, yawRotation, 0.0f);
+
+
+
+
+
+  }
 }
